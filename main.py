56bc032074b640f6bfc621d76b204f756a549e47
jazzbeau <jazzbeaumcinnes@gmail.com>
2025-02-26 12:02:36 +1030

    add deltatime and 60fps tick to game loop

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ def main():
     print(f"Screen height: {cn.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((cn.SCREEN_WIDTH, cn.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # game loop
     while True:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        # Store time in seconds within dt, side effect set to 60fps using .tick()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
